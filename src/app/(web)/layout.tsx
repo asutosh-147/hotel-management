e58f1f9d8eb876c13b025fb3977ad019699d08cb
asutosh-147 <98374996+asutosh-147@users.noreply.github.com>
2024-04-04 07:16:30 +0530

    second

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -42,7 +42,6 @@ export default function RootLayout({
             <main className='font-normal'>
               <Header />
               {children}
-              <Footer />
             </main>
           </ThemeProvider>
         </NextAuthProvider>
--- src/components/NewsLetter/NewsLetter.tsx
+++ src/components/NewsLetter/NewsLetter.tsx
@@ -1,6 +1,6 @@
 const NewsLetter = () => {
   return (
-    <section className='container mx-auto px-4'>
+    <section className='container mx-auto px-4 mb-10'>
       <form className='bg-primary text-white px-4 rounded-xl md:rounded-[30px] flex flex-col justify-center items-center py-6 md:py-24'>
         <p className='md:font-semibold text-lg md:text-xl text-center mb-3'>
           Explore More About Our Hotel
